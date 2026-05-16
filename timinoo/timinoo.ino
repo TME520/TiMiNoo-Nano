002a6822959f00c3cdb8138f92ba8d1eb56162c2
TME520 <tme520@gmail.com>
2026-05-16 19:07:12 +1000

    WIP: v1.4.1 (3)

--- timinoo/timinoo.ino
+++ timinoo/timinoo.ino
@@ -832,16 +832,12 @@ void drawDialog(int mode, const char* text, const char* helper, int icon) {
     return;
   }
 
-  // u8g.drawLine(0, 8, 127, 8);
-  u8g.drawLine(0, 56, 127, 56);
-
-  // u8g.setFont(u8g_font_baby);
   u8g.setFont(u8g2_font_ncenB08_tr);
   int textX = (128 - u8g.getStrWidth(text)) / 2;
   if (textX < 0) {
     textX = 0;
   }
-  u8g.drawStr(textX, 18, text);
+  u8g.drawStr(textX, 10, text);
 
   switch (icon) {
     case 0:
@@ -870,6 +866,7 @@ void drawDialog(int mode, const char* text, const char* helper, int icon) {
       break;
   }
 
+  u8g.drawLine(0, 56, 127, 56);
   u8g.setFont(u8g_font_baby);
   u8g.drawStr(10, 63, helper);
 }
